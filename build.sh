44d5289c26951d58f6d21acba56338ddb10ecdcb
grant-musick <grant@cozi.com>
2017-06-13 12:32:39 -0700

    testing get of protected resource

--- build.sh
+++ build.sh
@@ -3,4 +3,5 @@ cp ./app/*.py ./bld/
 pip install requests -t ./bld/
 pip install skillful -t ./bld/
 cd ./bld
+wget https://s3.amazonaws.com/grant-codebuild-output-bucket/output.tar/output.tar
 zip -r ../lambda_deploy.zip .
